30c1412741db7089288d16dc417fde876939d9a0
Seryazi-Andela <phillip.seryazi@gmail.com>
2019-07-17 12:02:37 +0300

    Feature(profile): add ability to retrieve user profile [Finishes #167122791]

--- stackoverflow/apps/users/urls.py
+++ stackoverflow/apps/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from .views import RegistrationView, LoginView, LogoutView
-
+from .views import (RegistrationView, LoginView, LogoutView, UserProfileView)
 
 urlpatterns = [
     path('signup/', RegistrationView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
