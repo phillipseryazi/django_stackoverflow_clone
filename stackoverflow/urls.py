bff6192bb70f403413f68cf0f05749f4e70051f6
Seryazi-Andela <phillip.seryazi@gmail.com>
2019-07-29 10:51:26 +0300

    Chore(collectstatic):collecting static for swagger

--- stackoverflow/urls.py
+++ stackoverflow/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
@@ -38,4 +40,4 @@ urlpatterns = [
     path('api/v1/questions/', include('stackoverflow.apps.questions.urls')),
     path('api/v1/questions/comments/', include('stackoverflow.apps.comments.urls')),
     path('api/v1/questions/answers/', include('stackoverflow.apps.answers.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
